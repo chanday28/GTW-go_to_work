dfe02b5ce7cc3dd48bd3a77a1d6a51ba2e33dc1d
Feizen <msifeizen@gmail.com>
2020-07-30 15:01:12 +0300

    Juri
    Paralax added and fixed

--- Assets/Scripts/parallax.cs
+++ Assets/Scripts/parallax.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class parallax : MonoBehaviour
 {
-    public GameObject BG;
-    public float speed;
+    //public GameObject BG;
+    //public float speed;
+
+    //private void FixedUpdate()
+    //{
+    //    transform.position = new Vector2(transform.position.x + 1f * Time.deltaTime * speed, transform.position.y);
+    //}
+    private float length, startPos;
+    [SerializeField]
+    private GameObject cam;
+    public float parallaxFX;
+
+    private void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
 
     private void FixedUpdate()
     {
-        transform.position = new Vector2(transform.position.x + 1f * Time.deltaTime * speed, transform.position.y);
-    }
+        float temp = (cam.transform.position.x * (1 - parallaxFX));
+        float distance = (cam.transform.position.x * parallaxFX);
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
 
+        if (temp > startPos + length) startPos += length;
+        else if (temp < startPos - length) startPos -= length;
+    }
 }
